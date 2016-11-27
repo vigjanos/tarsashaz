e30ad0123bde21752d0c5483f8259912bed86c00
vigjanos <vigjanos62@gmail.com>
2016-11-27 21:49:28 +0100

    tarh_konyvel_view.xml-ben a lakláseladásnál a nyitott flag readonly!
    egyszeri előírások létrehozásánál a Törlés megvalósítása, törli a tarh_lako_eloiras és a tarh_lakoeloir havi
    táblákból is !

--- tarh_konyvel/tarh_egyszeribeir.py
+++ tarh_konyvel/tarh_egyszeribeir.py
@@ -17,10 +17,11 @@ class tarh_egyszeribeir(osv.osv):
                                      required=True),
         'tulajdonos': fields.many2one('res.partner', 'Tulajdonos', required=True),
         'idopont': fields.date('Előírás időpontja', required=True),
-        'eloirfajta': fields.many2one('eloiras.fajta', 'Eloiras tipusa', required=True),
+        'eloirfajta': fields.many2one('eloiras.fajta', 'Előírás típusa', required=True),
         'osszeg': fields.integer('Előírás összege', required=True),
-        'lezart': fields.boolean(),
-        'torolt': fields.boolean(),
+        'lezart': fields.boolean('Lezárva'),
+        'torolt': fields.boolean('Törölve'),
+        'torolte': fields.integer('ID aki törölte')
     }
     _defaults = {
         'lezart': False,
@@ -86,6 +87,93 @@ class tarh_egyszeribeir(osv.osv):
             raise osv.except_orm(_("Figyelem!!!"), _(" Le van zarva, nem modosithato!"))
 
         pass
+
+    def torles (self, cr, uid, ids, context):
+        '''
+        Ez az eljárás törli a bevitt előírást a tarh_eloiras_lako táblából, valamint megkeresi és törli az előírást a
+        tarh_lakoeloir_havi táblából is!
+        '''
+        hiba1 = ''
+        hiba2 = ''
+        _tarh_eloiras_lako = self.pool.get('tarh.eloiras.lako')
+        _tarh_lakoeloir_havi = self.pool.get('tarh.lakoeloir.havi')
+        if self.browse(cr, uid, ids, context).torolt == False and self.browse(cr, uid, ids, context).lezart == True:
+            # Ha még nincs törölve, de már le van zárva, akkor megkeressük a tarh_eloiras_lako
+            # táblában azt a bejegyzést, amelyet ez alapján a rekord alapján jegyeztünk be
+            idopont = str_to_date(self.browse(cr, uid, ids, context).idopont)
+            _eloir_datum = idopont
+            _esedekes = idopont.day
+            _lako = self.browse(cr, uid, ids, context).tulajdonos.id
+            _eloirfajta = self.browse(cr, uid, ids, context).eloirfajta.id
+            _osszeg = self.browse(cr, uid, ids, context).osszeg
+            _eloir_kezd = date(idopont.year, idopont.month, 1)
+            _eloir_vege = honap_utolsonap(idopont)
+            bejegyzesek1 = _tarh_eloiras_lako.search(cr, uid, [('esedekes', '=', _esedekes), ('lako', '=', _lako),
+                                                               ('eloirfajta', '=', _eloirfajta),
+                                                               ('osszeg', '=', _osszeg),
+                                                               ('eloir_kezd', '=', _eloir_kezd),
+                                                               ('eloir_vege', '=', _eloir_vege)], context=None)
+            if bejegyzesek1:
+                _tarh_eloiras_lako.unlink(cr, uid, bejegyzesek1, context=None)
+            else:
+                hiba1 = 'Nem találtam bejegyzést a lakó előírásai között!!'
+
+            # megkeressük a tarh_lakoeloir_havi táblában ezt a bejegyzést, amit ez alapján a rekord alapján jegyeztünk be
+            bejegyzesek2 = _tarh_lakoeloir_havi.search(cr, uid,
+                                                       [('eloir_datum', '=', _eloir_datum),
+                                                        ('lako', '=', _lako),
+                                                        ('eloirfajta', '=', _eloirfajta),
+                                                        ('osszeg', '=', _osszeg)], context=None)
+            if bejegyzesek2:
+                _tarh_lakoeloir_havi.unlink(cr, uid, bejegyzesek2, context=None)
+            else:
+                hiba2 = ' Nem találtam bejegyzést a lakó havi előírásai között!!'
+
+            if hiba1 == '' and hiba2 == '':
+                # mind a két táblából sikerült a törlés :-)
+                beirni = {
+                    'torolt': True,
+                    'torolte': uid
+                }
+                self.write(cr, 1, ids, beirni)
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'action_warn',
+                    'name': 'Growl',
+                    'params': {
+                        'title': 'Figyelem!',
+                        'text': 'A bejegyzés törlésre került!!',
+                        'sticky': True
+                    }
+                }
+            elif hiba1 != '' or hiba2 != '':
+                beirni = {
+                    'torolt': True,
+                    'torolte': uid
+                }
+                self.write(cr, 1, ids, beirni)
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'action_warn',
+                    'name': 'Growl',
+                    'params': {
+                        'title': 'Figyelem!',
+                        'text': hiba1 + hiba2 + ' Amit találtam, töröltem!',
+                        'sticky': True
+                    }
+                }
+
+        else:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'action_warn',
+                'name': 'Growl',
+                'params': {
+                    'title': 'Figyelem!',
+                    'text': 'A bejegyzés már törölve van, vagy még nincs lezárva!',
+                    'sticky': True
+                }
+            }
 
 
 tarh_egyszeribeir()
