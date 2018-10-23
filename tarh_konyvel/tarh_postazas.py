033e8c86c4113c4f5bda4c5c4a040ea0868930b0
vigjanos <vigjanos62@gmail.com>
2018-10-23 12:07:32 +0200

    A víz, hőmennyiség stb. mérőórák nyilvántartása, a mérőállások rögzítése

--- tarh_konyvel/tarh_postazas.py
+++ tarh_konyvel/tarh_postazas.py
@@ -56,3 +56,42 @@ class Fenymasolas(models.Model):
                                 domain="[('is_company','=',True),('name','ilike','%rsash%')]")
     oldalszam = fields.Integer('Oldalszám')
     megjegyzes = fields.Char('Megjegyzés')
+
+class Mero_ora(models.Model):
+    _name = ('tarh.mero.ora')
+    _order = 'tarsashaz, tulajdonos'
+
+    name = fields.Char('Gyári szám')
+    tarsashaz = fields.Many2one('res.partner', string='Társasház', required="True",
+                                domain="[('is_company','=',True),('name','ilike','%rsash%')]")
+    tulajdonos = fields.Many2one('res.partner', string='Tulajdonos', required='True')
+    ora_tipus = fields.Selection([('hidegviz', 'Hidegvíz'),
+                                  ('melegviz','Melegvíz'),
+                                  ('homenny','Hőmennyiség'),
+                                  ('elekt','Elektromos')],
+                                 default='hidegviz',
+                                 string='Óra típus')
+    felszerelve = fields.Date('Felszerelve')
+    ervenyes = fields.Date('Érvényesség')
+    aktiv = fields.Boolean(string='Aktív', default='True')
+
+class Oraallas_rogzites(models.Model):
+    _name = 'tarh.oraallas.rogzites'
+    _order = 'datum desc'
+
+    tarsashaz = fields.Many2one('res.partner', string='Társasház', required="True",
+                                domain="[('is_company','=',True),('name','ilike','%rsash%')]")
+    tulaj = fields.Many2one('res.partner', string='Tulajdonos', required='True')
+    mero_ora = fields.Many2one('tarh.mero.ora', string='mérőóra')
+    ora_allas = fields.Integer('Óraállás')
+    oratipus = fields.Char('Óratípus')
+    datum = fields.Date('Dátum')
+    olvasott = fields.Boolean('Leolvasott érték', default='True')
+
+    @api.onchange('mero_ora')
+    def mero(self):
+        print self.mero_ora.ora_tipus
+        self.oratipus = self.mero_ora.ora_tipus
+        pass
+        return()
+
